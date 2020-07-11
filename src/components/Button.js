7fcc19b92dd8b35cf84e7f07b601fcf74b54a5e8
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-07-11 06:09:25 +0100

    add styling for Button component and used props for variant styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,6 +5,24 @@ import PropTypes from 'prop-types';
 const Button = props => {
   const { name, color, wide } = props;
 
+  const greyColor = () => (color ? { backgroundColor: '#e0e0e0' } : { backgroundColor: '#f5913e' });
+
+  const wideWidth = () => (wide ? { flexBasis: '50%' } : { flexBasis: '25%' });
+
+  const baseStyle = {
+    outline: 'none',
+    border: '2px solid #d1d1d1',
+    fontSize: '2rem',
+    cursor: 'pointer',
+
+    ':hover': {
+      transform: 'scale(1.02)',
+      border: '3px solid #d0d9f1',
+      color: '#333',
+    },
+  };
+
+  const assignButtonStyles = { ...baseStyle, ...greyColor(), ...wideWidth() };
 
   return (
     <button type="button" className="button" style={assignButtonStyles}>
