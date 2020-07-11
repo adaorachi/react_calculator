f5651b7ed3dd1aa4e7413e24a492d1245385e722
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-07-11 06:06:26 +0100

    add styling for Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Display = props => {
-
+  const displayArea = {
+    height: '100px',
+    fontWeight: 'bolder',
+    color: '#fff',
+    backgroundColor: '#858694',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 1rem',
+  };
 
   const { value } = props;
   return (
