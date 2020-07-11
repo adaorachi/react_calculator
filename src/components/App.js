34c0d3358b4f5fa8c54351a564de2baaeff91f45
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-07-11 03:20:35 +0100

    create App.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
 
 function App() {
   return (
     <div className="App">
       <h1>React Calculator</h1>
+      <Display />
+      <ButtonPanel />
     </div>
   );
 }
