60a1801b1299d2f4cce77be1177de46daf2ab5ba
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-07-12 13:12:28 +0100

    create operate module/method using Big.js package

---
+++ src/logic/operate.js
@@ -0,0 +1,30 @@
+import big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  const numOneEval = big(numberOne);
+  const numTwoEval = big(numberTwo);
+  let finalOpr;
+  switch (operation) {
+    case '+':
+      finalOpr = numOneEval.plus(numTwoEval).toString();
+      break;
+    case '-':
+      finalOpr = numOneEval.minus(numTwoEval).toString();
+      break;
+    case 'x':
+      finalOpr = numOneEval.times(numTwoEval).toString();
+      break;
+    case '÷':
+      finalOpr = numOneEval.div(numTwoEval).toString();
+      break;
+    case '%':
+      finalOpr = numOneEval.div(100).toString();
+      break;
+    default:
+      finalOpr = null;
+      break;
+  }
+  return finalOpr;
+};
+
+export default operate;
