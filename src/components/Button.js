b0676900b70c59b3293946d51f6796972a98291a
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-07-12 13:38:34 +0100

    implement handleclick function for App component & passed to ButtonPanel component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = props => {
-  const { name, color, wide } = props;
+  const {
+    name, color, wide,
+  } = props;
 
   const greyColor = (color === 'grey') ? { backgroundColor: '#e0e0e0' } : { backgroundColor: '#f5913e' };
   const wideWidth = wide ? { flexBasis: '50%' } : { flexBasis: '25%' };
